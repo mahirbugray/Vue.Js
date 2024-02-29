99745a70575113928a3928bc2489fd3c14902e33
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2024-02-29 23:42:21 +0300

    Update c

---
+++ 09-Vue-Router/router-app/src/views/StudentInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Öğrenci Bilgi Sayfası</h3>
+    <!-- <p>Öğrenci ID: {{$route.params.id}}</p> -->
+    <p>Öğrenci ID: {{id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['id']
+  // data() {
+  //   return {
+  //     studentID: this.$route.params.id
+  //   }
+  // },     burada tanımlayıp studentID yi içeri vererek de çağırabiliriz.
+}
+</script>
+
+<style>
+
+</style>
